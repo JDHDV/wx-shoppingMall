e25c0e2900b7d28bbd495ce157c8f123476c4006
HGZhen <1073991028@qq.com>
2017-12-18 20:10:29 +0800

    wx for shoppingMall

---
+++ app.js
@@ -0,0 +1,123 @@
+//app.js
+App({
+  onLaunch: function () {
+    var that = this;
+    //  获取商城名称
+    wx.request({
+      url: 'https://api.it120.cc/'+ that.globalData.subDomain +'/config/get-value',
+      data: {
+        key: 'mallName'
+      },
+      success: function(res) {
+        if (res.data.code == 0) {
+          wx.setStorageSync('mallName', res.data.data.value);
+        }
+      }
+    })
+    this.login();
+  },
+  //登录
+  login : function () {
+    var that = this;
+    var token = that.globalData.token;
+    if (token) {
+      wx.request({
+        url: 'https://api.it120.cc/' + that.globalData.subDomain + '/user/check-token',
+        data: {
+          token: token
+        },
+        success: function (res) {
+          if (res.data.code != 0) {
+            that.globalData.token = null;
+            that.login();
+          }
+        }
+      })
+      return;
+    }
+    wx.login({
+      success: function (res) {
+        wx.request({
+          url: 'https://api.it120.cc/'+ that.globalData.subDomain +'/user/wxapp/login',
+          data: {
+            code: res.code
+          },
+          success: function(res) {
+            if (res.data.code == 10000) {
+              // 去注册
+              that.registerUser();
+              return;
+            }
+            if (res.data.code != 0) {
+              // 登录错误
+              wx.hideLoading();
+              wx.showModal({
+                title: '提示',
+                content: '无法登录，请重试',
+                showCancel:false
+              })
+              return;
+            }
+            //console.log(res.data.data)
+            that.globalData.token = res.data.data.token;
+            that.globalData.uid = res.data.data.uid;
+          }
+        })
+      }
+    })
+  },
+  registerUser: function () {
+    var that = this;
+    wx.login({
+      success: function (res) {
+        var code = res.code; // 微信登录接口返回的 code 参数，下面注册接口需要用到
+        wx.getUserInfo({
+          success: function (res) {
+            var iv = res.iv;
+            var encryptedData = res.encryptedData;
+            console.log(res.encryptedData)
+            // 下面开始调用注册接口
+            wx.request({
+              url: 'https://api.it120.cc/' + that.globalData.subDomain +'/user/wxapp/register/complex',
+              data: {code:code,encryptedData:encryptedData,iv:iv}, // 设置请求的 参数
+              success: (res) =>{
+                wx.hideLoading();
+                that.login();
+              }
+            })
+          }
+        })
+      }
+    })
+  },
+  sendTempleMsg: function (orderId, trigger, template_id, form_id, page, postJsonString){
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + that.globalData.subDomain + '/template-msg/put',
+      method:'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        token: that.globalData.token,
+        type:0,
+        module:'order',
+        business_id: orderId,
+        trigger: trigger,
+        template_id: template_id,
+        form_id: form_id,
+        url:page,
+        postJsonString: postJsonString
+      },
+      success: (res) => {
+        //console.log(res.data);
+      }
+    })
+  },
+  globalData:{
+    userInfo:null,
+    subDomain: "39c5a628a8bad2d8895d94b60fadf6ff",
+    version: "1.7",
+    shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
+  }
+})
